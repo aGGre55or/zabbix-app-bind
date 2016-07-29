ece2c693a1d7ca1a27b033f3e1503a7104a5826f
aGGre55or <amitrans@narod.ru>
2016-07-29 13:13:02 +0300

    script

--- named-stats.sh
+++ named-stats.sh
@@ -24,27 +24,27 @@ if [ -s ${STATS} ]; then
     named_dropped_query=$(awk '/ queries dropped/ {print $1}' $STATS)
     
     if [ "$METRIC" = "named_total_query" ]; then 
-	if ! [ -z $named_total_query ]; then echo $named_total_query; else echo "0"; fi
+	if ! [ -z $named_total_query ]; then echo $named_total_query; else echo 0; fi
     fi
     
     if [ "$METRIC" = "named_ipv4_request" ]; then 
-	if ! [ -z $named_ipv4_request ]; then echo $named_ipv4_request; else echo "0"; fi
+	if ! [ -z $named_ipv4_request ]; then echo $named_ipv4_request; else echo 0; fi
     fi
     
     if [ "$METRIC" = "named_tcp_request" ]; then 
-	if ! [ -z $named_tcp_request ]; then echo $named_tcp_request; else echo "0"; fi
+	if ! [ -z $named_tcp_request ]; then echo $named_tcp_request; else echo 0; fi
     fi
     
     if [ "$METRIC" = "named_total_response" ]; then 
-	if ! [ -z $named_total_response ]; then echo $named_total_response; else echo "0"; fi
+	if ! [ -z $named_total_response ]; then echo $named_total_response; else echo 0; fi
     fi
     
     if [ "$METRIC" = "named_success_query" ]; then 
-	if ! [ -z $named_success_query ]; then echo $named_success_query; else echo "0"; fi
+	if ! [ -z $named_success_query ]; then echo $named_success_query; else echo 0; fi
     fi
 
     if [ "$METRIC" = "named_dropped_query" ]; then 
-	if ! [ -z $named_dropped_query ]; then echo $named_dropped_query; else echo "0"; fi
+	if ! [ -z $named_dropped_query ]; then echo $named_dropped_query; else echo 0; fi
     fi
 
 elif tty >/dev/null ; then
