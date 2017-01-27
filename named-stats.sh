eb099171830c26ebe6c2c2c20a45187701e7bc75
aGGre55or <amitrans@narod.ru>
2017-01-27 12:58:30 +0300

    named-stats.sh optimization

--- named-stats.sh
+++ named-stats.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# oev <offer4job@outlook.com>; 27.07.2016
+# oev <offer4job@outlook.com>; 27.01.2017
 
 if [[ -z "$1" ]]; then
      exit 1
@@ -22,30 +22,22 @@ if [ -s ${STATS} ]; then
     named_total_response=$(awk '/[0-9] responses sent/ {print $1}' $STATS)
     named_success_query=$(awk '/queries resulted in successful/ {print $1}' $STATS)
     named_dropped_query=$(awk '/ queries dropped/ {print $1}' $STATS)
-    
-    if [ "$METRIC" = "named_total_query" ]; then 
-	if ! [ -z $named_total_query ]; then echo $named_total_query; else echo 0; fi
-    fi
-    
-    if [ "$METRIC" = "named_ipv4_request" ]; then 
-	if ! [ -z $named_ipv4_request ]; then echo $named_ipv4_request; else echo 0; fi
-    fi
-    
-    if [ "$METRIC" = "named_tcp_request" ]; then 
-	if ! [ -z $named_tcp_request ]; then echo $named_tcp_request; else echo 0; fi
-    fi
-    
-    if [ "$METRIC" = "named_total_response" ]; then 
-	if ! [ -z $named_total_response ]; then echo $named_total_response; else echo 0; fi
-    fi
-    
-    if [ "$METRIC" = "named_success_query" ]; then 
-	if ! [ -z $named_success_query ]; then echo $named_success_query; else echo 0; fi
-    fi
 
-    if [ "$METRIC" = "named_dropped_query" ]; then 
-	if ! [ -z $named_dropped_query ]; then echo $named_dropped_query; else echo 0; fi
-    fi
+case "$METRIC" in
+  "named_total_query")    if ! [ -z $named_total_query ]; then echo $named_total_query; else echo 0; fi
+                          ;;
+  "named_ipv4_request")   if ! [ -z $named_ipv4_request ]; then echo $named_ipv4_request; else echo 0; fi
+                          ;;
+  "named_tcp_request")    if ! [ -z $named_tcp_request ]; then echo $named_tcp_request; else echo 0; fi
+                          ;;
+  "named_total_response") if ! [ -z $named_total_response ]; then echo $named_total_response; else echo 0; fi
+                          ;;
+  "named_success_query")  if ! [ -z $named_success_query ]; then echo $named_success_query; else echo 0; fi
+                          ;;
+  "named_dropped_query")  if ! [ -z $named_dropped_query ]; then echo $named_dropped_query; else echo 0; fi
+                          ;;
+  *) ;;
+esac
 
 elif tty >/dev/null ; then
     echo "${STATS} does not exist!" >&2
@@ -53,3 +45,4 @@ elif tty >/dev/null ; then
 fi
 
 exit 0
+
